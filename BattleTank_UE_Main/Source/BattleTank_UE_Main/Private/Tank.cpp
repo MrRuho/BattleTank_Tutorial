58e0553c6aa9975a478063472f9fe05fc850b71a
Paajanen Ville <paajanen.ville@gmail.com>
2018-10-13 11:01:50 +0300

    Aiming Component addet to Tank (C++File)

--- BattleTank_UE_Main/Source/BattleTank_UE_Main/Private/Tank.cpp
+++ BattleTank_UE_Main/Source/BattleTank_UE_Main/Private/Tank.cpp
@@ -9,32 +9,29 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
-	Super::BeginPlay();
-	
+	Super::BeginPlay();	
 }
 
 // Called every frame
 void ATank::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
 void ATank::AimAt(FVector OutHitLocation)
 {
-	auto OurTankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aming at %s"),*OurTankName, *OutHitLocation.ToString());
+	TankAimingComponent->AimAt(OutHitLocation);
 }
 
