223b88833677480cbd7f83e0b2c3358a5119918e
Paajanen Ville <paajanen.ville@gmail.com>
2018-10-17 09:58:41 +0300

    Tahtays ja ammuksen lahto suunta asennettu.

--- BattleTank_UE_Main/Source/BattleTank_UE_Main/Private/TankAimingComponent.cpp
+++ BattleTank_UE_Main/Source/BattleTank_UE_Main/Private/TankAimingComponent.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Kismet/GameplayStatics.h"
 #include "TankAimingComponent.h"
 
 
@@ -39,7 +40,29 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 
 void UTankAimingComponent::AimAt(FVector OutHitLocation, float LaunchSpeed)
 {
-	
-	UE_LOG(LogTemp, Warning, TEXT("Firing at %F"), LaunchSpeed);
+	if (!Barrel) { return;}
+
+	FVector OutLaunchVelocity;
+	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+	if(UGameplayStatics::SuggestProjectileVelocity
+		(
+			this, 
+			OutLaunchVelocity,
+			StartLocation,
+			OutHitLocation, 
+			LaunchSpeed, 
+			false, 
+			0, 
+			0, 
+			ESuggestProjVelocityTraceOption::DoNotTrace 
+			)
+		)
+
+
+	{
+		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+		auto TankName = GetOwner()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("%s aiming at %F"),*TankName, *AimDirection.ToString());
+	}
 }
 
