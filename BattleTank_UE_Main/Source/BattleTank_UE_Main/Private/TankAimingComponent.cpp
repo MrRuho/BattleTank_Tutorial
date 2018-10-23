a0138523e4bfb3491a68db44385d265d33260a9e
Paajanen Ville <paajanen.ville@gmail.com>
2018-10-23 11:25:03 +0300

    Revert "Tahtays ja ammuksen lahto suunta asennettu."
    This reverts commit 223b88833677480cbd7f83e0b2c3358a5119918e.

--- BattleTank_UE_Main/Source/BattleTank_UE_Main/Private/TankAimingComponent.cpp
+++ BattleTank_UE_Main/Source/BattleTank_UE_Main/Private/TankAimingComponent.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Kismet/GameplayStatics.h"
 #include "TankAimingComponent.h"
 
 
@@ -22,6 +21,7 @@ void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent * BarrelToSet
 
 void UTankAimingComponent::AimAt(FVector OutHitLocation, float LaunchSpeed)
 {
+<<<<<<< HEAD
 	if (!Barrel) { return;}
 
 	FVector OutLaunchVelocity;
@@ -43,6 +43,10 @@ void UTankAimingComponent::AimAt(FVector OutHitLocation, float LaunchSpeed)
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
 	}
+=======
+	
+	UE_LOG(LogTemp, Warning, TEXT("Firing at %F"), LaunchSpeed);
+>>>>>>> parent of 223b888... Tahtays ja ammuksen lahto suunta asennettu.
 }
 
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
